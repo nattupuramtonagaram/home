16c0a17d05dfe4ef12f83eec09fd073148e11d8e
abinesh subramani <abineshsiva191@gmail.com>
2020-09-06 20:24:33 +0530

    Updates

---
+++ precache-manifest.2eb4dce04d21e009566a48e16ecdb060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ed26013428ca88c831cd2bf73f5a3e",
+    "url": "/landing/index.html"
+  },
+  {
+    "revision": "5e87a85fe9f49a6a6b9b",
+    "url": "/landing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23c4ea1f9698d6e7420b",
+    "url": "/landing/static/js/2.bb175f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing/static/js/2.bb175f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e87a85fe9f49a6a6b9b",
+    "url": "/landing/static/js/main.23f1a026.chunk.js"
+  },
+  {
+    "revision": "c7b2fc2db9c09d6585ab",
+    "url": "/landing/static/js/runtime-main.f0c0ae33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/landing/static/media/logo.5d5d9eef.svg"
+  }
+]);
